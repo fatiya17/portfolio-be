0435b6487737bc79f7696fd8967c0eb679b6add1
fatiya17 <fatiyalabibah17@gmail.com>
2026-01-04 22:24:03 +0700

    feat: implement API resources for Experience

---
+++ models/Experience.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const experienceSchema = new mongoose.Schema({
+  role: String,
+  company: String,
+  location: String,
+  period: String,
+  type: String,
+  description: String,
+  skills: [String] 
+});
+
+module.exports = mongoose.model('Experience', experienceSchema);
