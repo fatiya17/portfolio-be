5238bb39ba0616983026bb6cd886f2584cb83be0
fatiya17 <fatiyalabibah17@gmail.com>
2026-01-07 01:03:41 +0700

    feat: add message for backend live

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.get('/', (req, res) => {
+  res.send('🦉 Backend for Fatiya’s Portfolio Is Live!');
+});
 
 // connect db
 mongoose.connect(process.env.MONGO_URI)
@@ -171,3 +174,5 @@ app.delete('/api/skills/:id', async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+module.exports = app;
