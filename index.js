395eb09c82894bdf29159c81f162d15925040695
fatiya17 <fatiyalabibah17@gmail.com>
2026-02-19 19:58:27 +0700

    fix: error vercel after update education

--- index.js
+++ index.js
@@ -316,7 +316,6 @@ app.get("/api/education", async (req, res) => {
 });
 app.post("/api/education", async (req, res) => {
   try {
-    console.log(`POST /api/education body size: ${JSON.stringify(req.body).length}`);
     const body = req.body;
     // upload image if exists
     if (body.imageUrl) {
@@ -331,9 +330,7 @@ app.post("/api/education", async (req, res) => {
 });
 app.put("/api/education/:id", async (req, res) => {
   try {
-    console.log(`PUT /api/education/${req.params.id} body size: ${JSON.stringify(req.body).length}`);
     const body = req.body;
-    console.log("Updating Education ID:", req.params.id, "Image present:", !!body.imageUrl); // Debug log
     // upload image if changed
     if (body.imageUrl) {
       body.imageUrl = await uploadToCloudinary(body.imageUrl, "education");
